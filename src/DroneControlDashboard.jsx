20d4067afd5c5ac272217bbd3924d0bb89558d59
Hirw Hope <gakundohope5@gmail.com>
2024-06-02 10:15:35 +0200

    updates

--- src/DroneControlDashboard.jsx
+++ src/DroneControlDashboard.jsx
@@ -1,47 +1,48 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { FaPlay, FaStop, FaHome, FaArrowLeft, FaArrowRight, FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import io from 'socket.io-client';
-import {APIProvider, Map,Pin,AdvancedMarker} from '@vis.gl/react-google-maps';
-
-import 'leaflet/dist/leaflet.css';
+import { APIProvider, Map, Pin, AdvancedMarker, useMap } from '@vis.gl/react-google-maps';
 import Swal from 'sweetalert2';
 import { GiDeliveryDrone } from "react-icons/gi";
-const server = 'http://localhost:5000'
+import MapContainer from "./DroneLocation"
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
+const server = 'http://localhost:5000';
 const socket = io(server);
+
 const DroneControlDashboard = () => {
-  const position = {lat: -1.6006031230495232, lng: 29.515005049259088}
+  const position = { lat: -1.6006031230495232, lng: 29.515005049259088 };
   const [launchedDrones, setLaunchedDrones] = useState([]);
   const [droneId, setDroneId] = useState('');
+  const [showPopup, setShowPopup] = useState(false); // State to control popup visibility
   const [messages, setMessages] = useState([]);
   const [selectedDrone, setSelectedDrone] = useState(null);
+  const map = useMap(); // State to hold the map instance
 
   useEffect(() => {
-  
     socket.on('adminNotification', (data) => {
-      if(data.length!=0){
-      setLaunchedDrones(data);
+      if (data.length !== 0) {
+        setLaunchedDrones(data);
       }
     });
-    
+
     socket.on('new_message', (message) => {
       setMessages((prevMessages) => [...prevMessages, message]);
+      setShowPopup(true); // Show popup with new message
     });
-    
+
     return () => {
       socket.off('connect');
       socket.off('disconnect');
       socket.off('adminNotification');
-      // socket.off('error_message');
-      // socket.off('message_sent');
     };
   }, []);
 
-  const sendCommand = (command) => {
+  const sendCommand = (command,msg) => {
     if (droneId) {
       Swal.fire({
         title: 'Are you sure?',
-        text: `Send command '${command}' to drone ${droneId}?`,
+        text: `You want  to '${msg}' on drone ${droneId}?`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonText: 'Yes, send it!',
@@ -57,13 +58,39 @@ const DroneControlDashboard = () => {
     }
   };
 
-const sendControlCommand = (command)=>{
-  if(droneId){
-    socket.emit('sendCommand', { drone_id: droneId, command: command });
-  }
-}
+  const sendControlCommand = (command) => {
+    if (droneId) {
+      socket.emit('sendCommand', { drone_id: droneId, command: command });
+    }
+  };
+
+  const handleClick = useCallback((ev) => {
+    if (!map) return;
+    if (!ev.latLng) return;
+    console.log('marker clicked:', ev.latLng.toString());
+    map.panTo(ev.latLng);
+  }, [map]);
+
+  const handleMapClick = (event) => {
+    console.log("Map clicked at:", event.latLng.toString());
+    // Additional functionality can be added here, such as adding a marker at the clicked location
+  };
+
   return (
     <div className="container mx-auto p-6">
+    {showPopup && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 w-[25rem] mx-auto">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <button
+              className="mt-4 hover:text-blue-600 text-gray-500 font-bold py-2 px-4 rounded"
+              onClick={() => setShowPopup(false)}
+            >
+              <IoMdCloseCircleOutline size={16} />
+            </button>
+            <p className="text-lg flex gap-2">{messages[messages.length - 1].message} on <GiDeliveryDrone size={25}/> </p>
+          </div>
+        </div>
+      )}
       <div className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">Automatically Launched Drones</h2>
         <div className="overflow-x-auto">
@@ -78,49 +105,46 @@ const sendControlCommand = (command)=>{
               </tr>
             </thead>
             <tbody>
-  {launchedDrones.length !== 0 ? launchedDrones.map((drone, index) => (
-    <tr key={index}>
-      <td className="border px-4 py-2">
-        <GiDeliveryDrone size={25} />
-      </td>
-      <td className="border px-4 py-2">{drone.drone_id}</td>
-      <td className="border px-4 py-2">{drone.district ? drone.district.name : 'N/A'}</td>
-      <td className="border px-4 py-2">{drone.district?.latitude ?? 'N/A'}</td>
-      <td className="border px-4 py-2">{drone.district?.longitude ?? 'N/A'}</td>
-    </tr>
-  )) : ""}
-</tbody>
-
-
+              {launchedDrones.length !== 0 ? launchedDrones.map((drone, index) => (
+                <tr key={index}>
+                  <td className="border px-4 py-2">
+                    <GiDeliveryDrone size={25} />
+                  </td>
+                  <td className="border px-4 py-2">{drone.drone_id}</td>
+                  <td className="border px-4 py-2">{drone.district ? drone.district.name : 'N/A'}</td>
+                  <td className="border px-4 py-2">{drone.district?.latitude ?? 'N/A'}</td>
+                  <td className="border px-4 py-2">{drone.district?.longitude ?? 'N/A'}</td>
+                </tr>
+              )) : <></>}
+            </tbody>
           </table>
         </div>
       </div>
       <div className="mb-8 h-[50vh] w-full">
-        {/* <h2 className="text-2xl font-semibold mb-4">Real-time Drone Positions</h2> */}
-        <APIProvider apiKey={'AIzaSyBvvU6jaJbByhHnsqL2xrCT3FprNdyG6kc'}>
-        {/* <Map defaultCenter={position} center={position} zoom={10}>
-          {launchedDrones.map((drone, index) => (
-            <Marker
-              key={index}
-              options={{
-                position: { lat: parseFloat(drone.district.latitude), lng: parseFloat(drone.district.longitude) },
-              }}
-            />
-          ))}
-        </Map> */}
-        <Map
-            defaultZoom={16}
-            defaultCenter={position}
-            mapId='DEMO_MAP_ID'>
-            {launchedDrones.map((drone,index)=>(
-              <AdvancedMarker
-              key={index}
-              position={ {lat: parseFloat(drone.district.latitude), lng: parseFloat(drone.district.longitude) }}>
-            <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
-            </AdvancedMarker>
-            ))}
-        </Map>
-        </APIProvider>
+      {!selectedDrone ? (
+  <APIProvider apiKey={'AIzaSyBvvU6jaJbByhHnsqL2xrCT3FprNdyG6kc'}>
+    <Map
+      defaultZoom={7}
+      defaultCenter={position}
+      mapId='DEMO_MAP_ID'
+      onClick={handleMapClick} // Add click handler for the map
+    >
+      {launchedDrones.map((drone, index) => (
+        <AdvancedMarker
+          clickable={true}
+          onClick={handleClick}
+          key={index}
+          position={{ lat: parseFloat(drone.district.latitude), lng: parseFloat(drone.district.longitude) }}
+        >
+          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+        </AdvancedMarker>
+      ))}
+    </Map>
+  </APIProvider>
+) : (
+  <MapContainer origin={{lat: -1.5977107241293707, lng: 29.51424132530372}} destination={{lat:parseFloat(selectedDrone.district.latitude),lng:parseFloat(selectedDrone.district.longitude)}} />
+)}
+
       </div>
 
       <div>
@@ -128,49 +152,53 @@ const sendControlCommand = (command)=>{
         <form className="bg-white p-6 rounded-lg shadow-md">
           <div className="flex justify-center text-center py-4">
             <label htmlFor="droneId" className="block text-gray-700 font-bold p-2">Drone ID: </label>
-            <select 
-                id="droneId" 
-                name="droneId" 
-                value={droneId} 
-                onChange={(e) => {
-                setDroneId(e.target.value);
-                setSelectedDrone(e.target.value);
-                }} 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-[10rem]" 
-                required>
-                  <option value="">Select Drone</option>
-                    {launchedDrones.map((drone, index) => (
-                  <option key={index} value={drone.drone_id}>{drone.drone_id}</option>
-                ))}
-            </select>
+            <select
+  id="droneId"
+  name="droneId"
+  value={droneId}
+  onChange={(e) => {
+    const selectedDroneObject = launchedDrones.find(drone => drone.drone_id === e.target.value);
+    setSelectedDrone(selectedDroneObject);
+    setDroneId(selectedDroneObject.drone_id);
+  }}
+  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-[10rem]"
+  required
+>
+  <option value="">Select Drone</option>
+  {launchedDrones.map((drone, index) => (
+    <option key={index} value={drone.drone_id}>{drone.drone_id}</option>
+  ))}
+</select>
+            <span className='py-2'>or Type</span>
+            <input type="text" value={droneId} placeholder='Drone ID' className='border-2 rounded mx-2 border-gray-500' onChange={(e)=>setDroneId(e.target.value)} />
           </div>
           <div className="grid gap-4">
             <div className='grid grid-cols-3 gap-4'>
-              <button type="button" onClick={() => sendCommand('S')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Start">
+              <button type="button" onClick={() => sendCommand('S',"Disable Autopilote")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Start">
                 <FaPlay size={32} />
               </button>
-              <button type="button" onClick={() => sendCommand('T')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Stop">
+              <button type="button" onClick={() => sendCommand('T',"Enable Autopilote")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Stop">
                 <FaStop size={32} />
               </button>
-              <button type="button" onClick={() => sendCommand('H')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Home">
+              <button type="button" onClick={() => sendCommand('H',"Stop surveillance  and Return To station")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Home">
                 <FaHome size={32} />
               </button>
             </div>
             <div className='min-h-[30vh] max-h-[40vh] mx-auto aspect-square grid grid-cols-3 grid-rows-3 gap-8'>
               <div></div>
-              <button type="button" onClick={() => sendControlCommand('F')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Forward">
+              <button type="button" onClick={() => sendControlCommand('F')} className="bg-blue-500 hover:bg-blue-700 active:scale-110 active:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Forward">
                 <FaArrowUp size={27} />
               </button>
               <div></div>
-              <button type="button" onClick={() => sendControlCommand('L')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Left">
+              <button type="button" onClick={() => sendControlCommand('L')} className="bg-blue-500 hover:bg-blue-700 active:scale-110 active:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Left">
                 <FaArrowLeft size={27} />
               </button>
               <div className='bg-blue-500 aspect-square rounded-full'></div>
-              <button type="button" onClick={() => sendControlCommand('R')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Right">
+              <button type="button" onClick={() => sendControlCommand('R')} className="bg-blue-500 hover:bg-blue-700 active:scale-110 active:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Right">
                 <FaArrowRight size={27} />
               </button>
               <div></div>
-              <button type="button" onClick={() => sendControlCommand('B')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Backward">
+              <button type="button" onClick={() => sendControlCommand('B')} className="bg-blue-500 hover:bg-blue-700 active:scale-110 active:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center" title="Backward">
                 <FaArrowDown size={27} />
               </button>
             </div>
@@ -180,13 +208,41 @@ const sendControlCommand = (command)=>{
 
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-4">Message Notifications</h2>
-        <div className="bg-white p-4 rounded-lg shadow-md max-h-64 overflow-y-auto">
-          {messages.map((message, index) => (
-            <div key={index} className="border-b border-gray-200 py-2">
-              <p>{message}</p>
-            </div>
-          ))}
-        </div>
+        <div className="bg-white p-4 rounded-lg shadow-md">
+  <div className="overflow-x-auto">
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            No
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            From
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Message
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {messages.map((message, index) => (
+          <tr key={index}>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div className="text-sm text-gray-900">{index+1}</div>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div className="text-sm text-gray-900">{message.from}</div>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div className="text-sm text-gray-900">{message.message}</div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+
       </div>
     </div>
   );
--- src/DroneFeeds.jsx
+++ src/DroneFeeds.jsx
@@ -1,17 +1,21 @@
-
+import MapContainer from "./DroneLocation"
 export default function DroneFeeds() {
+    const origin = {lat: -1.5977107241293707, lng: 29.51424132530372}; // New York City
+    const destination = {lat: -1.5977107241293707, lng: 29.71424132530372}; // Los Angeles
   return (
     <div>
 <div className="bg-white w-full h-full h-min-screen p-8">
     <div className="container m-auto">
         <div className="w-full z-0 relative md:min-h-[41rem]">
             <div className="w-full h-full relative z-0 rounded-2xl">
-                <video className="relative z-[1] inline w-full h-full object-center object-cover rounded-2xl" preload="metadata" data-src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607" loop="" playsinline="" autoplay="" aria-hidden="false" src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607"></video>
-                <video className="absolute top-0 left-0 w-full h-full transform-gpu translate-x-0 translate-y-0 z-0 inline object-center object-cover rounded-2xl blur-2xl" preload="none" aria-hidden="false" data-src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607" loop="" playsinline="" autoplay="" src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607"></video>
+                <video className="relative z-[1] inline w-full h-full object-center object-cover rounded-2xl" preload="metadata" data-src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607" loop="" playsInline="" autoPlay="" aria-hidden="false" src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607"></video>
+                <video className="absolute top-0 left-0 w-full h-full transform-gpu translate-x-0 translate-y-0 z-0 inline object-center object-cover rounded-2xl blur-2xl" preload="none" aria-hidden="false" data-src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607" loop="" playsInline="" autoPlay="" src="https://player.vimeo.com/progressive_redirect/playback/840627949/rendition/1080p/file.mp4?loc=external&amp;signature=df9cfec6465233ea6f14bd85906ac7412e35a0a00e65cdb6823b45d2b2cf5607"></video>
             </div>
         </div>
     </div>
 </div>
+    <div className="h-[50vh] w-[95vw] mx-auto">
+        <MapContainer origin={origin} destination={destination} />    </div>
     </div>
   )
 }
